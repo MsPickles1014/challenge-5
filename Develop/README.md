f073ea1ddbfd4240d5e999d614589514d77460ca
Ms Pickles1014 <177317399+MsPickles1014@users.noreply.github.comgit config --global user.emailgit config --global user.email noeladnelson@gmail.com>
2024-11-26 17:37:39 -0500

    Index-JS initial code

---
+++ Develop/README.md
@@ -0,0 +1,35 @@
+
+# professional ReadMe
+
+## Description
+This app creates  effectively and efficiently ReadMe pages for Github.
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+Run `npm install` to install dependencies.
+
+## Usage
+Use `node index.js` to run the application.
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+ Contributions are welcome! Submit a pull request.
+
+## Tests
+Run tests using `npm test`.
+
+## Questions
+For questions, please contact me via:
+- GitHub: [MsPickles1014](https://github.com/MsPickles1014)
+- Email: [noeladnelson@gmail.com](mailto:noeladnelson@gmail.com)
+    
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,115 @@
 // TODO: Include packages needed for this application
+import fs from 'fs';
+import inquirer from 'inquirer';
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What is the usage information?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What are the contribution guidelines?',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'What are the test instructions?',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your project:',
+        choices: ['MIT', 'GPLv3', 'Apache 2.0', 'BSD 3-Clause', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// TODO: Create a function to generate the README content
+function writeToFile(data) {
+    return `
+# ${data.title}
 
-// TODO: Create a function to initialize app
-function init() {}
+## Description
+${data.description}
 
-// Function call to initialize app
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## License
+This project is licensed under the ${data.license} license.
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+For questions, please contact me via:
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: [${data.email}](mailto:${data.email})
+    `;
+}
+
+// TODO: Create a function to initialize the app
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            // Generate the README content
+            const readmeContent = writeToFile(answers);
+
+            // Write the README file
+            fs.writeFile('README.md', readmeContent, (err) =>
+                err
+                    ? console.error('Error writing file:', err)
+                    : console.log('README.md generated successfully!')
+            );
+        })
+        .catch((error) => {
+            console.error('Error during initialization:', error);
+        });
+}
+
+// Function call to initialize the app
 init();
